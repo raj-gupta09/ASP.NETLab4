faebf5bad73ba566752230df849635596ca0fd56
raj-gupta09 <rajk.gupta0910@gmail.com>
2018-10-31 09:29:10 +0530

    Add files via upload

---
+++ Lab4/Lab4/PageLifeCycleDemo.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PageLifeCycleDemo.aspx.cs" Inherits="Lab4.PageLifeCycleDemo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Lab4/Lab4/PageLifeCycleDemo.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab4
+{
+    public partial class PageLifeCycleDemo : System.Web.UI.Page
+    {
+        String msg = "Events in Page Life Cycle in ASP.NET" + "<BR>";
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            msg = msg + "<BR>" + "Pre Init";
+        }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            msg = msg + "<BR>" + "Init";
+        }
+
+        protected void Page_InitComplete(object sender, EventArgs e)
+        {
+            msg = msg + "<BR>" + "Init Complete";
+        }
+
+        protected void Page_PreLoad(object sender, EventArgs e)
+        {
+            msg = msg + "<BR>" + "Pre Load";
+        }
+
+        protected void Page_LoadComplete(object sender, EventArgs e)
+        {
+            msg = msg + "<BR>" + "Load Complete";
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            msg = msg + "<BR>" + "Load";
+        }
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            msg = msg + "<BR>" + "Pre Render";
+            Label1.Text = msg;
+        }
+
+    }
+}
